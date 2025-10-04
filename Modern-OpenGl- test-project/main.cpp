0669d744cb41c8c2f4892d4cee3e6d1353e632f8
MadugodageGT <gtharaka784@gmail.com>
2025-10-04 21:19:54 +0530

    feat(camera) : implemented camera movment through user inputs (keyboard+mouse)

--- Modern-OpenGl- test-project/main.cpp
+++ Modern-OpenGl- test-project/main.cpp
@@ -118,6 +118,8 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		shaderProgram.Activate();
 
+
+		camera.Inputs(window);
 		camera.Matrix(45.0f, 0.1f, 100.0f,shaderProgram, "camMatrix");
 
 
