1e61a0ed0abb2e4ffe4bac6b8c648c0365052706
MadugodageGT <gtharaka784@gmail.com>
2025-10-27 11:48:59 +0530

    add drone class

--- Modern-OpenGl- test-project/Drone.cpp
+++ Modern-OpenGl- test-project/Drone.cpp
@@ -1,10 +1,9 @@
-#include "Mesh.h"
+#include "Drone.h"
 
-Drone::Drone(std::vector <Vertex>& vertices, std::vector <GLuint>& indices, std::vector <Texture>& textures)
+Drone::Drone(std::vector <Vertex>& vertices, std::vector <GLuint>& indices)
 {
 	Drone::vertices = vertices;
 	Drone::indices = indices;
-	Drone::textures = textures;
 
 	VAO.Bind();
 	// Generates Vertex Buffer Object and links it to vertices
@@ -28,26 +27,6 @@ void Drone::Draw(Shader& shader, Camera& camera)
 	// Bind shader to be able to access uniforms
 	shader.Activate();
 	VAO.Bind();
-
-	// Keep track of how many of each type of textures we have
-	unsigned int numDiffuse = 0;
-	unsigned int numSpecular = 0;
-
-	for (unsigned int i = 0; i < textures.size(); i++)
-	{
-		std::string num;
-		std::string type = textures[i].type;
-		if (type == "diffuse")
-		{
-			num = std::to_string(numDiffuse++);
-		}
-		else if (type == "specular")
-		{
-			num = std::to_string(numSpecular++);
-		}
-		textures[i].texUnit(shader, (type + num).c_str(), i);
-		textures[i].Bind();
-	}
 
 	glUniform3f(glGetUniformLocation(shader.ID, "camPos"), camera.Position.x, camera.Position.y, camera.Position.z);
 	camera.Matrix(shader, "camMatrix");
--- Modern-OpenGl- test-project/Drone.h
+++ Modern-OpenGl- test-project/Drone.h
@@ -1,24 +1,22 @@
-#ifndef MESH_CLASS_H
-#define MESH_CLASS_H
+#ifndef DRONE_CLASS_H
+#define DRONE_CLASS_H
 
 #include<string>
 
 #include"VAO.h"
 #include"EBO.h"
 #include"Camera.h"
-#include"Texture.h"
 
 class Drone
 {
 public:
 	std::vector <Vertex> vertices;
 	std::vector <GLuint> indices;
-	std::vector <Texture> textures;
 	// Store VAO in public so it can be used in the Draw function
 	VAO VAO;
 
 	// Initializes the mesh
-	Drone(std::vector <Vertex>& vertices, std::vector <GLuint>& indices, std::vector <Texture>& textures);
+	Drone(std::vector <Vertex>& vertices, std::vector <GLuint>& indices);
 
 	// Draws the mesh
 	void Draw(Shader& shader, Camera& camera);
